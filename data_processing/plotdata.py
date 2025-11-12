3b29d3295cf603cd24422816327c62e63679305f
ns14 <65001356+ns14@users.noreply.github.com>
2025-11-12 10:55:54 -0800

    Update plotdata.py

--- data_processing/plotdata.py
+++ data_processing/plotdata.py
@@ -1,21 +1,22 @@
+import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
 def plot_results(state, cov):
-  t = np.linspace(0, len(state))
+  t = np.arange(len(state))
   x = np.array([matrix[0] for matrix in state])
   y = np.array([matrix[1] for matrix in state])
   z = np.array([matrix[2] for matrix in state])
   x_cov = np.array([matrix[0, 0] for matrix in cov])
   y_cov = np.array([matrix[1, 1] for matrix in cov])
   z_cov = np.array([matrix[2, 2] for matrix in cov])
-  x_up = x + 1.96 * x_cov
-  x_low = x - 1.96 * x_cov
-  y_up = y + 1.96 * y_cov
-  y_low = y - 1.96 * y_cov
-  z_up = z + 1.96 * z_cov
-  z_low = z - 1.96 * z_cov
+  x_up = x + 1.96 * np.sqrt(x_cov)
+  x_low = x - 1.96 * np.squrt(x_cov)
+  y_up = y + 1.96 * np.sqrt(y_cov)
+  y_low = y - 1.96 * np.sqrt(y_cov)
+  z_up = z + 1.96 * np.sqrt(z_cov)
+  z_low = z - 1.96 * np.sqrt(z_cov)
 
+  #plot x position vs. time
   plt.figure()
   plt.title("x position estimate vs. time")
   plt.xlabel("t")
@@ -25,20 +26,22 @@ def plot_results(state, cov):
   plt.plot(t, x_low, color='cyan', linestyle='--', linewidth=1)
   plt.fill_between(t, x_up, x_low, color='gray', alpha=0.2)
 
+  #plot y position vs. time
   plt.figure()
   plt.title("y position estimate vs. time")
   plt.xlabel("t")
   plt.ylabel("y")
-  plt.plot(t, z, linestyle='-', color='blue', linewidth=1)
-  plt.plot(t, z_up, color='cyan', linestyle='--', linewidth=1)
-  plt.plot(t, z_low, color='cyan', linestyle='--', linewidth=1)
-  plt.fill_between(t, z_up, z_low, color='gray', alpha=0.2)
-
-  plt.figure()
-  plt.title("z position estimate vs. time")
-  plt.xlabel("t")
-  plt.ylabel("z")
   plt.plot(t, y, linestyle='-', color='blue', linewidth=1)
   plt.plot(t, y_up, color='cyan', linestyle='--', linewidth=1)
   plt.plot(t, y_low, color='cyan', linestyle='--', linewidth=1)
   plt.fill_between(t, y_up, y_low, color='gray', alpha=0.2)
+
+  #plot z position vs. time
+  plt.figure()
+  plt.title("z position estimate vs. time")
+  plt.xlabel("t")
+  plt.ylabel("z")
+  plt.plot(t, z, linestyle='-', color='blue', linewidth=1)
+  plt.plot(t, z_up, color='cyan', linestyle='--', linewidth=1)
+  plt.plot(t, z_low, color='cyan', linestyle='--', linewidth=1)
+  plt.fill_between(t, z_up, z_low, color='gray', alpha=0.2)
